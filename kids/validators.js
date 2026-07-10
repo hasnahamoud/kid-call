b9a353b230c8baa45db1b37cbc0888e6c0ac992a
hasnahamoud <hamoudhasna3@gmail.com>
2026-07-10 21:13:56 +0300

    Add admin endpoint to confirm kid

--- kids/validators.js
+++ kids/validators.js
@@ -42,3 +42,29 @@ export function validateGetKidsOf(req, res, next){
 
     next();
 }
+export function validateConfirmKid(req, res, next) {
+    const kid_id = req.params.id;
+
+    const schema = Joi.object({
+        kid_id: Joi.number()
+            .integer()
+            .positive()
+            .required()
+    });
+
+    const { error } = schema.validate({
+        kid_id
+    });
+
+    if (error) {
+        const messages = error.details.map(detail => detail.message);
+
+        throw new AppError(
+            messages.join(","),
+            400,
+            error
+        );
+    }
+
+    next();
+}
