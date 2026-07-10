d2e14dbe29eeb834cc40319d94dc041dd8ac59e6
hasnahamoud <hamoudhasna3@gmail.com>
2026-07-10 20:37:35 +0300

    Add admin route for listing all kids

--- kids/urls.js
+++ kids/urls.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { addKid, getKidsOf } from './kids.js';
+import { addKid, getKidsOf, getAllKids } from './kids.js';
 import { validateAddingKid, validateGetKidsOf } from './validators.js';
 
 export const router = express.Router();
 
 router.post('/', validateAddingKid, addKid);
 
-router.get('/:id', validateGetKidsOf, getKidsOf);
 
+router.get('/admin/all', getAllKids);
+
+router.get('/:id', validateGetKidsOf, getKidsOf);
